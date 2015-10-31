d534bf3a1d929f928baed4db3c0fd87b26e6e5fd
Andrei Nevar <andrei_nevar@nl.ibm.com>
2015-10-31 03:27:16 +0100

    initial commit

---
+++ SwipeViewDemo/LoginScreenController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginScreenController.swift
+//  SwipeViewDemo
+//
+//  Created by Andrei Nevar on 31/10/15.
+//  Copyright © 2015 Fructose Tech. All rights reserved.
+//
+
+import Foundation
